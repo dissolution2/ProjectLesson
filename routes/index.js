1a2013c446252e97cfbd354f1aa25b29c8edc472
Robin Larsen <larsen.robin75@gmail.com>
2024-03-03 14:51:23 +0100

    added auth ?

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,10 @@ router.get('/', async function(req, res, next) {
     Delimiter: '/',
     Prefix: 'public/'
   };
+  
   var allObjects = await s3.listObjects(params).promise();
   var keys = allObjects?.Contents.map( x=> x.Key).slice(0,3);
+  
   const pictures = await Promise.all(keys.map(async (key) => {
     let my_file = await s3.getObject({
       Bucket: process.env.CYCLIC_BUCKET_NAME,
@@ -29,7 +31,11 @@ router.get('/', async function(req, res, next) {
         name: key.split("/").pop()
     }
   }))
-  res.render('index', { pictures: pictures, title: 'Express' });
+  res.render('index', { 
+    pictures: pictures, 
+    title: 'Express',
+    isAuthenticated: req.oidc.isAuthenticated()
+   });
 });
 
 
