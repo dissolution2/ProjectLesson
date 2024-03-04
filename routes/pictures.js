5351c185094c55bd92e1138711566e4ceed6d3b0
Robin Larsen <larsen.robin75@gmail.com>
2024-03-04 12:04:13 +0100

    test

--- routes/pictures.js
+++ routes/pictures.js
@@ -110,4 +110,31 @@ router.get('/', async function(req, res, next) {
 });
 
 
+router.get('/:name', async function(req, res, next) {
+
+  var params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: '/',
+    Prefix: 'public/'
+  };
+
+  var allObjects = await s3.listObjects(params).promise();
+  var keys = allObjects?.Contents.map(x => x.Key);
+
+  const pictures = await Promise.all(keys.map(async (key)=>{
+    let my_file = await s3.getObject({
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: key
+    }).promise();
+    return {
+      src: Buffer.from(my_file.Body.name).toString('base64'),
+      name: key.split("/").pop()
+    }
+  }))
+  
+
+
+  res.render('pictures', {pictures: pictures});
+});
+
 module.exports = router;
